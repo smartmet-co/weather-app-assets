d4ea9e3424e41369b1a644a1867f332ddfc594e8
Pekka Keränen <pekka.keranen@geosaaga.fi>
2025-05-13 09:59:29 +0300

    issue-860: Resolved merge conflict in index.ts

--- images/backgrounds/index.ts
+++ images/backgrounds/index.ts
@@ -1,377 +1,376 @@
 import { ImageSourcePropType } from "react-native";
-import { FullWindowOverlay } from "react-native-screens";
 
-export const backgroundsLight: WeatherBackground = {
+export const backgroundsPhone: WeatherBackground = {
   '1': {
-    day: require('./light/1.jpg'),
-    night: require('./light/101.jpg'),
+    day: require('./phone/1.jpg'),
+    night: require('./phone/101.jpg'),
   },
   '2': {
-    day: require('./light/2.jpg'),
-    night: require('./light/102.jpg'),
+    day: require('./phone/2.jpg'),
+    night: require('./phone/102.jpg'),
   },
   '4': {
-    day: require('./light/4.jpg'),
-    night: require('./light/104.jpg'),
+    day: require('./phone/4.jpg'),
+    night: require('./phone/104.jpg'),
   },
   '6': {
-    day: require('./light/6.jpg'),
-    night: require('./light/106.jpg'),
+    day: require('./phone/6.jpg'),
+    night: require('./phone/106.jpg'),
   },
   '7': {
-    day: require('./light/7.jpg'),
-    night: require('./light/107.jpg'),
+    day: require('./phone/7.jpg'),
+    night: require('./phone/107.jpg'),
   },
   '9': {
-    day: require('./light/9.jpg'),
-    night: require('./light/109.jpg'),
+    day: require('./phone/9.jpg'),
+    night: require('./phone/109.jpg'),
   },
   '11': {
-    day: require('./light/11.jpg'),
-    night: require('./light/111.jpg'),
+    day: require('./phone/11.jpg'),
+    night: require('./phone/111.jpg'),
   },
   '14': {
-    day: require('./light/14.jpg'),
-    night: require('./light/114.jpg'),
+    day: require('./phone/14.jpg'),
+    night: require('./phone/114.jpg'),
   },
   '17': {
-    day: require('./light/17.jpg'),
-    night: require('./light/117.jpg'),
+    day: require('./phone/17.jpg'),
+    night: require('./phone/117.jpg'),
   },
   '21': {
-    day: require('./light/21.jpg'),
-    night: require('./light/121.jpg'),
+    day: require('./phone/21.jpg'),
+    night: require('./phone/121.jpg'),
   },
   '24': {
-    day: require('./light/24.jpg'),
-    night: require('./light/124.jpg'),
+    day: require('./phone/24.jpg'),
+    night: require('./phone/124.jpg'),
   },
   '27': {
-    day: require('./light/27.jpg'),
-    night: require('./light/127.jpg'),
+    day: require('./phone/27.jpg'),
+    night: require('./phone/127.jpg'),
   },
   '31': {
-    day: require('./light/31.jpg'),
-    night: require('./light/131.jpg'),
+    day: require('./phone/31.jpg'),
+    night: require('./phone/131.jpg'),
   },
   '32': {
-    day: require('./light/32.jpg'),
-    night: require('./light/132.jpg'),
+    day: require('./phone/32.jpg'),
+    night: require('./phone/132.jpg'),
   },
   '33': {
-    day: require('./light/33.jpg'),
-    night: require('./light/133.jpg'),
+    day: require('./phone/33.jpg'),
+    night: require('./phone/133.jpg'),
   },
   '34': {
-    day: require('./light/34.jpg'),
-    night: require('./light/134.jpg'),
+    day: require('./phone/34.jpg'),
+    night: require('./phone/134.jpg'),
   },
   '35': {
-    day: require('./light/35.jpg'),
-    night: require('./light/135.jpg'),
+    day: require('./phone/35.jpg'),
+    night: require('./phone/135.jpg'),
   },
   '36': {
-    day: require('./light/36.jpg'),
-    night: require('./light/136.jpg'),
+    day: require('./phone/36.jpg'),
+    night: require('./phone/136.jpg'),
   },
   '37': {
-    day: require('./light/37.jpg'),
-    night: require('./light/137.jpg'),
+    day: require('./phone/37.jpg'),
+    night: require('./phone/137.jpg'),
   },
   '38': {
-    day: require('./light/38.jpg'),
-    night: require('./light/138.jpg'),
+    day: require('./phone/38.jpg'),
+    night: require('./phone/138.jpg'),
   },
   '39': {
-    day: require('./light/39.jpg'),
-    night: require('./light/139.jpg'),
+    day: require('./phone/39.jpg'),
+    night: require('./phone/139.jpg'),
   },
   '41': {
-    day: require('./light/41.jpg'),
-    night: require('./light/141.jpg'),
+    day: require('./phone/41.jpg'),
+    night: require('./phone/141.jpg'),
   },
   '42': {
-    day: require('./light/42.jpg'),
-    night: require('./light/142.jpg'),
+    day: require('./phone/42.jpg'),
+    night: require('./phone/142.jpg'),
   },
   '43': {
-    day: require('./light/43.jpg'),
-    night: require('./light/143.jpg'),
+    day: require('./phone/43.jpg'),
+    night: require('./phone/143.jpg'),
   },
   '44': {
-    day: require('./light/44.jpg'),
-    night: require('./light/144.jpg'),
+    day: require('./phone/44.jpg'),
+    night: require('./phone/144.jpg'),
   },
   '45': {
-    day: require('./light/45.jpg'),
-    night: require('./light/145.jpg'),
+    day: require('./phone/45.jpg'),
+    night: require('./phone/145.jpg'),
   },
   '46': {
-    day: require('./light/46.jpg'),
-    night: require('./light/146.jpg'),
+    day: require('./phone/46.jpg'),
+    night: require('./phone/146.jpg'),
   },
   '47': {
-    day: require('./light/47.jpg'),
-    night: require('./light/147.jpg'),
+    day: require('./phone/47.jpg'),
+    night: require('./phone/147.jpg'),
   },
   '48': {
-    day: require('./light/48.jpg'),
-    night: require('./light/148.jpg'),
+    day: require('./phone/48.jpg'),
+    night: require('./phone/148.jpg'),
   },
   '49': {
-    day: require('./light/49.jpg'),
-    night: require('./light/149.jpg'),
+    day: require('./phone/49.jpg'),
+    night: require('./phone/149.jpg'),
   },
   '51': {
-    day: require('./light/51.jpg'),
-    night: require('./light/missing.jpg'),
+    day: require('./phone/51.jpg'),
+    night: require('./phone/151.jpg'),
   },
   '52': {
-    day: require('./light/52.jpg'),
-    night: require('./light/152.jpg'),
+    day: require('./phone/52.jpg'),
+    night: require('./phone/152.jpg'),
   },
   '53': {
-    day: require('./light/53.jpg'),
-    night: require('./light/153.jpg'),
+    day: require('./phone/53.jpg'),
+    night: require('./phone/153.jpg'),
   },
   '54': {
-    day: require('./light/54.jpg'),
-    night: require('./light/154.jpg'),
+    day: require('./phone/54.jpg'),
+    night: require('./phone/154.jpg'),
   },
   '55': {
-    day: require('./light/55.jpg'),
-    night: require('./light/155.jpg'),
+    day: require('./phone/55.jpg'),
+    night: require('./phone/155.jpg'),
   },
   '56': {
-    day: require('./light/56.jpg'),
-    night: require('./light/156.jpg'),
+    day: require('./phone/56.jpg'),
+    night: require('./phone/156.jpg'),
   },
   '57': {
-    day: require('./light/57.jpg'),
-    night: require('./light/157.jpg'),
+    day: require('./phone/57.jpg'),
+    night: require('./phone/157.jpg'),
   },
   '58': {
-    day: require('./light/58.jpg'),
-    night: require('./light/158.jpg'),
+    day: require('./phone/58.jpg'),
+    night: require('./phone/158.jpg'),
   },
   '59': {
-    day: require('./light/59.jpg'),
-    night: require('./light/159.jpg'),
+    day: require('./phone/59.jpg'),
+    night: require('./phone/159.jpg'),
   },
   '61': {
-    day: require('./light/61.jpg'),
-    night: require('./light/161.jpg'),
+    day: require('./phone/61.jpg'),
+    night: require('./phone/161.jpg'),
   },
   '64': {
-    day: require('./light/64.jpg'),
-    night: require('./light/164.jpg'),
+    day: require('./phone/64.jpg'),
+    night: require('./phone/164.jpg'),
   },
   '67': {
-    day: require('./light/67.jpg'),
-    night: require('./light/167.jpg'),
+    day: require('./phone/67.jpg'),
+    night: require('./phone/167.jpg'),
   },
   '71': {
-    day: require('./light/71.jpg'),
-    night: require('./light/171.jpg'),
+    day: require('./phone/71.jpg'),
+    night: require('./phone/171.jpg'),
   },
   '74': {
-    day: require('./light/74.jpg'),
-    night: require('./light/174.jpg'),
+    day: require('./phone/74.jpg'),
+    night: require('./phone/174.jpg'),
   },
   '77': {
-    day: require('./light/77.jpg'),
-    night: require('./light/177.jpg'),
+    day: require('./phone/77.jpg'),
+    night: require('./phone/177.jpg'),
   },
   'aurora': {
-    day: require('./light/aurora.jpg'),
-    night: require('./light/aurora.jpg'),
+    day: require('./phone/aurora.jpg'),
+    night: require('./phone/aurora.jpg'),
   },
 };
 
-export const backgroundsDark: WeatherBackground = {
+export const backgroundsTablet: WeatherBackground = {
   '1': {
-    day: require('./dark/1.jpg'),
-    night: require('./dark/101.jpg'),
+    day: require('./tablet/1.jpg'),
+    night: require('./tablet/101.jpg'),
   },
   '2': {
-    day: require('./dark/2.jpg'),
-    night: require('./dark/102.jpg'),
+    day: require('./tablet/2.jpg'),
+    night: require('./tablet/102.jpg'),
   },
   '4': {
-    day: require('./dark/4.jpg'),
-    night: require('./dark/104.jpg'),
+    day: require('./tablet/4.jpg'),
+    night: require('./tablet/104.jpg'),
   },
   '6': {
-    day: require('./dark/6.jpg'),
-    night: require('./dark/106.jpg'),
+    day: require('./tablet/6.jpg'),
+    night: require('./tablet/106.jpg'),
   },
   '7': {
-    day: require('./dark/7.jpg'),
-    night: require('./dark/107.jpg'),
+    day: require('./tablet/7.jpg'),
+    night: require('./tablet/107.jpg'),
   },
   '9': {
-    day: require('./dark/9.jpg'),
-    night: require('./dark/109.jpg'),
+    day: require('./tablet/9.jpg'),
+    night: require('./tablet/109.jpg'),
   },
   '11': {
-    day: require('./dark/11.jpg'),
-    night: require('./dark/111.jpg'),
+    day: require('./tablet/11.jpg'),
+    night: require('./tablet/111.jpg'),
   },
   '14': {
-    day: require('./dark/14.jpg'),
-    night: require('./dark/114.jpg'),
+    day: require('./tablet/14.jpg'),
+    night: require('./tablet/114.jpg'),
   },
   '17': {
-    day: require('./dark/17.jpg'),
-    night: require('./dark/117.jpg'),
+    day: require('./tablet/17.jpg'),
+    night: require('./tablet/117.jpg'),
   },
   '21': {
-    day: require('./dark/21.jpg'),
-    night: require('./dark/121.jpg'),
+    day: require('./tablet/21.jpg'),
+    night: require('./tablet/121.jpg'),
   },
   '24': {
-    day: require('./dark/24.jpg'),
-    night: require('./dark/124.jpg'),
+    day: require('./tablet/24.jpg'),
+    night: require('./tablet/124.jpg'),
   },
   '27': {
-    day: require('./dark/27.jpg'),
-    night: require('./dark/127.jpg'),
+    day: require('./tablet/27.jpg'),
+    night: require('./tablet/127.jpg'),
   },
   '31': {
-    day: require('./dark/31.jpg'),
-    night: require('./dark/131.jpg'),
+    day: require('./tablet/31.jpg'),
+    night: require('./tablet/131.jpg'),
   },
   '32': {
-    day: require('./dark/32.jpg'),
-    night: require('./dark/132.jpg'),
+    day: require('./tablet/32.jpg'),
+    night: require('./tablet/132.jpg'),
   },
   '33': {
-    day: require('./dark/33.jpg'),
-    night: require('./dark/133.jpg'),
+    day: require('./tablet/33.jpg'),
+    night: require('./tablet/133.jpg'),
   },
   '34': {
-    day: require('./dark/34.jpg'),
-    night: require('./dark/134.jpg'),
+    day: require('./tablet/34.jpg'),
+    night: require('./tablet/134.jpg'),
   },
   '35': {
-    day: require('./dark/35.jpg'),
-    night: require('./dark/135.jpg'),
+    day: require('./tablet/35.jpg'),
+    night: require('./tablet/135.jpg'),
   },
   '36': {
-    day: require('./dark/36.jpg'),
-    night: require('./dark/136.jpg'),
+    day: require('./tablet/36.jpg'),
+    night: require('./tablet/136.jpg'),
   },
   '37': {
-    day: require('./dark/37.jpg'),
-    night: require('./dark/137.jpg'),
+    day: require('./tablet/37.jpg'),
+    night: require('./tablet/137.jpg'),
   },
   '38': {
-    day: require('./dark/38.jpg'),
-    night: require('./dark/138.jpg'),
+    day: require('./tablet/38.jpg'),
+    night: require('./tablet/138.jpg'),
   },
   '39': {
-    day: require('./dark/39.jpg'),
-    night: require('./dark/139.jpg'),
+    day: require('./tablet/39.jpg'),
+    night: require('./tablet/139.jpg'),
   },
   '41': {
-    day: require('./dark/41.jpg'),
-    night: require('./dark/141.jpg'),
+    day: require('./tablet/41.jpg'),
+    night: require('./tablet/141.jpg'),
   },
   '42': {
-    day: require('./dark/42.jpg'),
-    night: require('./dark/142.jpg'),
+    day: require('./tablet/42.jpg'),
+    night: require('./tablet/142.jpg'),
   },
   '43': {
-    day: require('./dark/43.jpg'),
-    night: require('./dark/143.jpg'),
+    day: require('./tablet/43.jpg'),
+    night: require('./tablet/143.jpg'),
   },
   '44': {
-    day: require('./dark/44.jpg'),
-    night: require('./dark/144.jpg'),
+    day: require('./tablet/44.jpg'),
+    night: require('./tablet/144.jpg'),
   },
   '45': {
-    day: require('./dark/45.jpg'),
-    night: require('./dark/145.jpg'),
+    day: require('./tablet/45.jpg'),
+    night: require('./tablet/145.jpg'),
   },
   '46': {
-    day: require('./dark/46.jpg'),
-    night: require('./dark/146.jpg'),
+    day: require('./tablet/46.jpg'),
+    night: require('./tablet/146.jpg'),
   },
   '47': {
-    day: require('./dark/47.jpg'),
-    night: require('./dark/147.jpg'),
+    day: require('./tablet/47.jpg'),
+    night: require('./tablet/147.jpg'),
   },
   '48': {
-    day: require('./dark/48.jpg'),
-    night: require('./dark/148.jpg'),
+    day: require('./tablet/48.jpg'),
+    night: require('./tablet/148.jpg'),
   },
   '49': {
-    day: require('./dark/49.jpg'),
-    night: require('./dark/149.jpg'),
+    day: require('./tablet/49.jpg'),
+    night: require('./tablet/149.jpg'),
   },
   '51': {
-    day: require('./dark/51.jpg'),
-    night: require('./dark/151.jpg'),
+    day: require('./tablet/51.jpg'),
+    night: require('./tablet/151.jpg'),
   },
   '52': {
-    day: require('./dark/52.jpg'),
-    night: require('./dark/152.jpg'),
+    day: require('./tablet/52.jpg'),
+    night: require('./tablet/152.jpg'),
   },
   '53': {
-    day: require('./dark/53.jpg'),
-    night: require('./dark/153.jpg'),
+    day: require('./tablet/53.jpg'),
+    night: require('./tablet/153.jpg'),
   },
   '54': {
-    day: require('./dark/54.jpg'),
-    night: require('./dark/154.jpg'),
+    day: require('./tablet/54.jpg'),
+    night: require('./tablet/154.jpg'),
   },
   '55': {
-    day: require('./dark/55.jpg'),
-    night: require('./dark/155.jpg'),
+    day: require('./tablet/55.jpg'),
+    night: require('./tablet/155.jpg'),
   },
   '56': {
-    day: require('./dark/56.jpg'),
-    night: require('./dark/156.jpg'),
+    day: require('./tablet/56.jpg'),
+    night: require('./tablet/156.jpg'),
   },
   '57': {
-    day: require('./dark/57.jpg'),
-    night: require('./dark/157.jpg'),
+    day: require('./tablet/57.jpg'),
+    night: require('./tablet/157.jpg'),
   },
   '58': {
-    day: require('./dark/58.jpg'),
-    night: require('./dark/158.jpg'),
+    day: require('./tablet/58.jpg'),
+    night: require('./tablet/158.jpg'),
   },
   '59': {
-    day: require('./dark/59.jpg'),
-    night: require('./dark/159.jpg'),
+    day: require('./tablet/59.jpg'),
+    night: require('./tablet/159.jpg'),
   },
   '61': {
-    day: require('./dark/61.jpg'),
-    night: require('./dark/161.jpg'),
+    day: require('./tablet/61.jpg'),
+    night: require('./tablet/161.jpg'),
   },
   '64': {
-    day: require('./dark/64.jpg'),
-    night: require('./dark/164.jpg'),
+    day: require('./tablet/64.jpg'),
+    night: require('./tablet/164.jpg'),
   },
   '67': {
-    day: require('./dark/67.jpg'),
-    night: require('./dark/167.jpg'),
+    day: require('./tablet/67.jpg'),
+    night: require('./tablet/167.jpg'),
   },
   '71': {
-    day: require('./dark/171.jpg'),
-    night: require('./dark/171.jpg'),
+    day: require('./tablet/71.jpg'),
+    night: require('./tablet/171.jpg'),
   },
   '74': {
-    day: require('./dark/74.jpg'),
-    night: require('./dark/174.jpg'),
+    day: require('./tablet/74.jpg'),
+    night: require('./tablet/174.jpg'),
   },
   '77': {
-    day: require('./dark/77.jpg'),
-    night: require('./dark/177.jpg'),
+    day: require('./tablet/77.jpg'),
+    night: require('./tablet/177.jpg'),
   },
   'aurora': {
-    day: require('./dark/aurora.jpg'),
-    night: require('./dark/aurora.jpg'),
+    day: require('./tablet/aurora.jpg'),
+    night: require('./tablet/aurora.jpg'),
   },
 };
 
@@ -594,10 +593,8 @@ export const weatherBackgroundKeyParser = (key: string) => {
   return parsedKey;
 };
 
-export const weatherBackgroundGetter = (key: string, dark: boolean, blue = false) => {
-  let symbolSet = backgroundsLight;
-  if (dark) symbolSet = backgroundsDark;
-  if (blue) symbolSet = backgroundsBlue;
+export const weatherBackgroundGetter = (key: string, tablet = false) => {
+  const symbolSet = tablet ? backgroundsTablet : backgroundsPhone;
 
   if (key === 'aurora') {
     return symbolSet.aurora.night;
