9369626c4c44986fb5383fc9dd902659fd49c82a
Pekka Keränen <pekka.keranen@geosaaga.fi>
2025-05-27 08:39:25 +0300

    issue-887: Added constants.ts file to control features for which DynamicConfig can't be used

---
+++ constants.ts
@@ -0,0 +1,3 @@
+// Settings that can't be configured in DynamicConfig, for example for non React files
+
+export const IOS_APP_GROUP = 'group.fi.fmi.mobileweather.settings'; // Should be empty if widgets are not used
