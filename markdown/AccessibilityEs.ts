b8c753cb9a0e78f8d567101e9652444c27af0ab8
Antti Rumpunen <antti.rumpunen@fmi.fi>
2026-04-09 13:45:27 +0300

    Added accessibility markdowns

--- markdown/AccessibilityEs.ts
+++ markdown/AccessibilityEs.ts
@@ -1,7 +1,36 @@
 const accessibilityEs = `
-# Accessibility
+# Accesibilidad a la aplicación
+Esta declaración de accesibilidad explica cómo la aplicación del Instituto de Hidrología, Meteorología y Estudios Ambientales (IDEAM) cumple con la Ley de Provisión de Servicios Digitales, qué deficiencias existen en la accesibilidad de la aplicación y cómo puedes enviarnos tus comentarios sobre problemas de accesibilidad.
 
-En español
+Esta aplicación es responsabilidad del Instituto de Hidrología, Meteorología y Estudios Ambientales (IDEAM). Queremos que la mayor cantidad posible de usuarios pueda acceder a nuestros servicios digitales. Tenemo en cuenta la accesibilidad en el desarrollo de los servicios digitales.
+
+## ¿Qué tan accesible es la aplicación?
+Esta aplicación cumple parcialmente con los criterios de accesibilidad requeridos por la ley en los niveles A y AA (WCAG 2.1). La aplicación presenta algunas deficiencias en accesibilidad, las cuales se describen con más detalle a continuación.
+
+## ¿Ha notado deficiencias en la accesibilidad?
+Estamos trabajando constantemente para mejorar la accesibilidad de la aplicación. Si descubre algún problema que no esté descrito en esta página, por favor háganoslo saber y haremos todo lo posible por solucionarlo. Respondremos lo antes posible o dentro de un máximo de dos semanas. Pueds contactarnos por correo electrónico a
+contacto@ideam.gov.co
+
+## Accessibility enforcement (TODO translation missing)
+La Agencia Regional Administrativa del Estado de Colombia tiene implementado  oficina de quejas relacionada con  los requisitos de accesibilidad. Si no estás satisfecho con la respuesta que recibe, o si no recibe respuesta dentro de dos semanas, puedes enviar tus comentarios a la Agencia Regional Administrativa del Estado. El sitio web de la Agencia Regional Administrativa del Estado tiene detalles sobre cómo presentar una queja y cómo se procesará el asunto.
+
+## Detalles sobre la accesibilidad técnica
+La aplicación aún no es completamente accesible.
+
+## El Contenido o las funciones aún no son accesibles
+> Algunos símbolos meteorológicos y barras de lluvia en la aplicación no tienen suficiente contraste con el fondo o con elementos adyacentes. (WCAG 1.4.3)
+
+> La aplicación tiene elementos que están agrupados, pero que pueden recibir el foco de manera separada. (WCAG 2.4.3)
+
+El objetivo es rectificar las deficiencias mencionadas aquí durante 2026-2027, si es posible, sin realizar cambios significativos en la apariencia o funcionalidad de la aplicación.
+
+## Accesibilidad del contenido basado en el mapa
+La aplicación proporciona información meteorológica basada en mapas. Según la legislación, los mapas y servicios de mapas no tienen que cumplir con los requisitos de accesibilidad. Los datos visualizados en los mapas están disponibles en otro formato accesible dentro de la aplicación.
+
+La aplicación fue publicada el 25 de mayo de 2026. 
+Esta declaración fue preparada el 9 de abril de 2026.
+
+(TODO do we need to add links to accessibility regulations or something?)
 `;
 
 export default accessibilityEs;
