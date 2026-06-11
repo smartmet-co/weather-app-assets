febc283a424b62ec762d9d8684312305a4115aee
Pekka Keränen <pekka.keranen@geosaaga.fi>
2026-06-11 12:06:09 +0300

    Added font definitions to constants.ts

--- constants.ts
+++ constants.ts
@@ -1,3 +1,9 @@
 // Settings that can't be configured in DynamicConfig, for example for non React files
 
-export const IOS_APP_GROUP = 'group.fi.fmi.mobileweather.settings'; // Should be empty if widgets are not used
+export const IOS_APP_GROUP = ''; // Should be empty if widgets are not used
+
+export const LIGHT_FONT = 'Roboto-Light';
+export const REGULAR_FONT = 'Roboto-Regular';
+export const MEDIUM_FONT = 'Roboto-Medium';
+export const BOLD_FONT = 'Roboto-Bold';
+export const SEMIBOLD_FONT = 'Roboto-SemiBold';
